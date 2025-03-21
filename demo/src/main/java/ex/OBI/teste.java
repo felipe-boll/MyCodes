36b18b5fc2f7baf5a045599d051d2904c38fee90
felipe-boll <felipechavesboll@gmail.com>
2025-03-21 14:53:15 -0300

    OBI(varsea)

--- demo/src/main/java/ex/OBI/teste.java
+++ demo/src/main/java/ex/OBI/teste.java
@@ -1,7 +1,19 @@
 package ex.OBI;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class teste {
+
+    public static Scanner tecladoScanner = new Scanner(System.in);
+    public static Random random = new Random();
+
     public static void main(String[] args) {
-        System.out.println("Hacker Mode");
+        int[] vetor = new int[10];
+
+        for(int i = 0; i < vetor.length; i++){
+            vetor[i] = random.nextInt();
+            System.out.println(vetor[i]);
+        }
     }
 }
