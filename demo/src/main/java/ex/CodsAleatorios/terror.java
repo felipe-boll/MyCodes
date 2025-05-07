6d5707fc1d5063593a56fbc2cdfc9c4c0210788d
felipe-boll <felipechavesboll@gmail.com>
2025-05-07 11:07:43 -0300

    Arrumando erro da prova e fazendo joguinho

---
+++ demo/src/main/java/ex/CodsAleatorios/terror.java
@@ -0,0 +1,49 @@
+package ex.CodsAleatorios;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class terror {
+    public static Scanner tecladoScanner = new Scanner(System.in);
+    public static Random random = new Random();
+
+    public static String[] reacoes(){
+        String[] reacoes = new String[5];
+        
+        reacoes[0] = "É só isso que voce tem?";
+        reacoes[1] = "Isso não assusta nem minha irma recem-nascida";
+        reacoes[2] = "uááá, que sono essa sua historia";
+        reacoes[3] = "Buá, buá, estou chorando de medo. Só que não";
+        reacoes[4] = "Da pra me liberar? Tenho coisas mais importantes que isso pra fazer do que escutar suas historias chatas";
+
+        return reacoes;
+    }
+
+    public static void medo(String historiaTerror, String[] reacoes){
+        String reacao = " ";
+
+        if (historiaTerror.equals("Buu!!!")) {
+            reacao = "AAAAAHHHHH!!!! QUE MEDO!!!\nDesito voce ganhou, só não faz isso de novo por favor";
+            System.out.printf("\n%s\n",reacao);
+        } else{
+            reacao = reacoes[random.nextInt(5)];
+            System.out.printf("\n%s\n",reacao);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Tente me assustar, mas duvido que vai conseguir, HAHAHAHAHAHA\n");
+        String[] reacoes = reacoes();
+        
+        while (true) {
+            String historiaTerror = tecladoScanner.nextLine();
+
+            medo(historiaTerror, reacoes);
+
+            if (historiaTerror.equals("Buu!!!")) {
+                break;
+            }
+
+        }
+    }
+}
