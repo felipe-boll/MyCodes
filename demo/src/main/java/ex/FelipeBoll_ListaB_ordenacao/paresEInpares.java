886ca50cba822e358cba2f6438a40ccaa503a3b5
Felipe Boll <felipechavesboll@gmail.com>
2025-06-02 20:59:26 -0300

    listB

--- demo/src/main/java/ex/FelipeBoll_ListaB_ordenacao/paresEInpares.java
+++ demo/src/main/java/ex/FelipeBoll_ListaB_ordenacao/paresEInpares.java
@@ -3,20 +3,57 @@ package ex.FelipeBoll_ListaB_ordenacao;
 import java.util.Scanner;
 
 public class paresEInpares {
-    
+
     public static Scanner tecladoScanner = new Scanner(System.in);
 
-    public static int[] imparesEParesOrdenados(int[] paresEImpares){
-        int tamanho = paresEImpares.length;
+    public static void trocar(int[] paresEImpares, int i) {
+        int ajuda = paresEImpares[i];
+        paresEImpares[i] = paresEImpares[i + 1];
+        paresEImpares[i + 1] = ajuda;
+    }
 
+    public static int[] imparesEParesOrdenados(int[] paresEImpares) {
+        int tamanho = paresEImpares.length - 1;
+        int[] paresEImparesOrdenados = new int[paresEImpares.length];
+        boolean troca = true;
 
-        for (int i = 0; i < tamanho; i++) {
-            int valor = tecladoScanner.nextInt();
+        while (troca) {
+            troca = false;
+
+            for (int i = 0; i < tamanho; i++) {
+                if (i == tamanho) {
+                } else {
+                    if (paresEImpares[i] > paresEImpares[i + 1]) {
+                        trocar(paresEImpares, i);
+                        troca = true;
+                    }
+                }
+            }
         }
 
+        for (int i = 0; i < paresEImpares.length; i++) {
+            paresEImparesOrdenados[i] = paresEImpares[i];
+        }
 
+        return paresEImparesOrdenados;
     }
-    
+
+    public static void imprimirPares(int[] paresEImpares) {
+        for (int i = 0; i < paresEImpares.length; i++) {
+            if (paresEImpares[i] % 2 == 0) {
+                System.out.println(paresEImpares[i]);
+            }
+        }
+    }
+
+    public static void imprimirImpares(int[] paresEImpares) {
+        for (int i = paresEImpares.length - 1; i > 0; i--) {
+            if (paresEImpares[i] % 2 != 0) {
+                System.out.println(paresEImpares[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int qntTeste = tecladoScanner.nextInt();
         int[] paresEImpares = new int[qntTeste];
@@ -26,5 +63,9 @@ public class paresEInpares {
         }
 
         paresEImpares = imparesEParesOrdenados(paresEImpares);
+
+        imprimirPares(paresEImpares);
+        imprimirImpares(paresEImpares);
+        System.out.println();
     }
 }
